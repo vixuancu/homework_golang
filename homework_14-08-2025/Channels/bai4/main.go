840415be9098c3d175f7b734bbab5f1a4ac1cf39
vixuancu <vixuancu2004@gmail.com>
2025-08-26 09:56:20 +0700

    test

--- homework_14-08-2025/Channels/bai4/main.go
+++ homework_14-08-2025/Channels/bai4/main.go
@@ -8,9 +8,21 @@ func sumNumbers(ch chan int) {
 	ch <- sum // Gửi kết quả vào kênh
 }
 func main() {
-	ch := make(chan int)
+	ch := make(chan int) // Tạo một kênh để nhận kết quả
 
 	go sumNumbers(ch) // Gọi hàm sumNumbers trong goroutine
 	result := <-ch // Nhận kết quả từ kênh
 	println("Tổng các số từ 1 đến 100 là:", result) 
 }
+/*
+Channel không có buffer (make(chan int)):
+Khi main gọi result := <-ch, nó sẽ block (chờ) cho đến khi có dữ liệu được gửi vào ch.
+Goroutine sumNumbers phải chạy xong vòng lặp và thực hiện ch <- sum thì main mới tiếp tục được.
+
+Cơ chế đồng bộ tự nhiên:
+Nhờ channel, goroutine sumNumbers và main được đồng bộ hóa.
+main không thể kết thúc trước khi nhận được giá trị từ goroutine (vì đang bị block).
+
+Nếu không có channel (hoặc không chờ):
+Nếu bạn bỏ dòng result := <-ch, thì main có thể chạy xong ngay và kết thúc chương trình, khiến goroutine chưa kịp tính toán.
+*/
--- homework_14-08-2025/Goroutines/bai2/main.go
+++ homework_14-08-2025/Goroutines/bai2/main.go
@@ -7,28 +7,28 @@ import (
 
 var (
 	balance int        = 1000 // Số dư tài khoản ban đầu
-	mu      sync.Mutex        // Khóa để đồng bộ truy cập vào biến balance
+	//mu      sync.Mutex        // Khóa để đồng bộ truy cập vào biến balance
 )
 
 func deposit(wg *sync.WaitGroup) {
 	defer wg.Done() // Đánh dấu goroutine đã hoàn thành khi kết thúc
 
-	for i := 0; i < 100; i++ {
-		mu.Lock()      // Khóa để đảm bảo an toàn khi truy cập vào balance
+	for i := 0; i < 10000; i++ {
+	//	mu.Lock()      // Khóa để đảm bảo an toàn khi truy cập vào balance
 		balance += 200 // Thêm 200 vào số dư
-		mu.Unlock()    // Mở khóa sau khi cập nhật xong
+	//	mu.Unlock()    // Mở khóa sau khi cập nhật xong
 	}
 
 }
 
 func withdraw(wg *sync.WaitGroup) {
 	defer wg.Done() // Đánh dấu goroutine đã hoàn thành khi kết thúc
-	for i := 0; i < 100; i++ {
-		mu.Lock()           // Khóa để đảm bảo an toàn khi truy cập vào balance
-		if balance >= 100 { // Kiểm tra xem có đủ tiền để rút không
+	for i := 0; i < 10000; i++ {
+		//mu.Lock()           // Khóa để đảm bảo an toàn khi truy cập vào balance
+		//if balance >= 100 { // Kiểm tra xem có đủ tiền để rút không
 			balance -= 100 // Rút 100 từ số dư
-		}
-		mu.Unlock() // Mở khóa sau khi cập nhật xong
+		//}
+		//mu.Unlock() // Mở khóa sau khi cập nhật xong
 	}
 
 }
